87bce9e41632179c52ad61a263f8b7a531a0fc7d
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-04-21 11:25:31 +0200

    feat: day-1 completato

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import PostsList from './pages/PostsList'
+import About from './pages/About'
 import './App.css'
 
 function App() {
 
   return (
     <>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/posts' element={<PostsList />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
 
     </>
   )
